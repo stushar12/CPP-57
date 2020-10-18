050a7081d34b3241f4e280024db765972a6444e9
Tushar Saxena <stushar345@gmail.com>
2020-10-18 12:16:02 +0530

    Add files via upload

---
+++ two.cpp
@@ -0,0 +1,14 @@
+bool detectLoop(Node* head)
+{
+    set<Node *> s;
+    while(head!=NULL)
+    {
+        if(s.find(head)!=s.end())
+        {
+            return true;
+        }
+            s.insert(head);
+            head=head->next;
+    }
+    return false;
+}
